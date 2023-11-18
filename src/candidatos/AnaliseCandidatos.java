025822a3167ea488eb1317aec1d188bd8b4f85d3
Rexline <103515200+Rexline@users.noreply.github.com>
2023-11-18 12:23:37 -0300

    Iseriondo esqueleto do projeto

---
+++ src/candidatos/AnaliseCandidatos.java
@@ -0,0 +1,25 @@
+package candidatos;
+
+public class AnaliseCandidatos {
+    public static void main(String[] args) {
+        analisarCandidato(1900.0);
+        analisarCandidato(2000.0);
+        analisarCandidato(2200.0);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+
+        if(salarioBase > salarioPretendido){
+            System.out.println("Ligar para o Candidato.");
+        }
+        if(salarioBase == salarioPretendido){
+            System.out.println("Ligar para o candidato com contra proposta.");
+        }
+        if (salarioBase < salarioPretendido){
+            System.out.println("Aguardadando resultado dos demais Candidatos.");
+        }
+    }
+
+
+}
